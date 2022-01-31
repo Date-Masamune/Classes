e7d05a11742e1ae5c35a78903017fbec144962f5
jacob gishwiller <sirbruce13@yahoo.com>
2022-01-31 08:04:08 -0700

    first commit

---
+++ Classes exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Classes_exercise
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
