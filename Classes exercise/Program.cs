23e156dbf544557855e0202be9108669e56e0ec4
jacob gishwiller <sirbruce13@yahoo.com>
2022-01-31 08:59:36 -0700

    added instance variables

--- Classes exercise/Program.cs
+++ Classes exercise/Program.cs
@@ -2,8 +2,13 @@
 
 namespace Classes_exercise
 {
-   class Program
+   class Invoice
    {
+      private string part_number; // instance variable for part_number
+      private string description; //instance variable for description
+      private string quantity; // instance variable for quantity
+      private string unit_price; // instance variable for unit_price
+
       static void Main(string[] args)
       {
          Console.WriteLine("Hello World!");
